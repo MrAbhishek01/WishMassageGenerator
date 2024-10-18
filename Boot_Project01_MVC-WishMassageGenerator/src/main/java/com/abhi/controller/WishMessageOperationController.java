9e5f2b97cf212cc090b1fb689c90875dcbb74cf4
Abhishek Gupta <abhisheksatna001@gmail.com>
2024-10-18 09:13:36 +0530

    updated code

--- Boot_Project01_MVC-WishMassageGenerator/src/main/java/com/abhi/controller/WishMessageOperationController.java
+++ Boot_Project01_MVC-WishMassageGenerator/src/main/java/com/abhi/controller/WishMessageOperationController.java
@@ -1,36 +1,43 @@
 package com.abhi.controller;
 
 import java.util.Date;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.abhi.service.IWishMessageGenerator;
 
 @Controller
 public class WishMessageOperationController {
-    
-    @Autowired
-    private IWishMessageGenerator service;
 
-    
-    @RequestMapping("/home")
-    public String msgController() {
-        // Maps to the welcome.jsp
-        return "welcome";
-    }
+	@Autowired
+	private IWishMessageGenerator service;
 
-    @RequestMapping("/wmg")
-    public ModelAndView fetchMessage(@RequestParam("name")String name) {
-        String generatedMessage = service.generateMessage(name);
-        ModelAndView mvw = new ModelAndView();
-        mvw.addObject("wMsg", generatedMessage);
-        mvw.addObject("sysdate", new Date());
-        mvw.setViewName("checkStatus");
-        
-        return mvw;
-    }
+	@RequestMapping("/home")
+	public String msgController() {
+		// Maps to the welcome.jsp
+		return "welcome";
+	}
+
+	/*@RequestMapping("/wmg")
+	public ModelAndView fetchMessage(@RequestParam("name")String name) {
+	    String generatedMessage = service.generateMessage(name);
+	    ModelAndView mvw = new ModelAndView();
+	    mvw.addObject("wMsg", generatedMessage);
+	    mvw.addObject("sysdate", new Date());
+	    mvw.setViewName("checkStatus");
+	    
+	    return mvw;
+	}*/
+	@RequestMapping("/wmg")
+	public String fetchMessage(@RequestParam("name") String name, Map<String, Object> mp) {
+		System.err.println(mp.getClass());
+		String generatedMessage = service.generateMessage(name);
+		mp.put("wMsg", generatedMessage);
+		mp.put("sysdate", new Date());
+		return "checkStatus";
+	}
 }
