15b2f3891a815afd8bd99fc594cfdd80817835da
Abhishek Gupta <abhisheksatna001@gmail.com>
2024-10-21 17:46:39 +0530

    updated Code with many Test Cases

--- Boot_Project01_MVC-WishMassageGenerator/src/main/java/com/abhi/controller/WishMessageOperationController.java
+++ Boot_Project01_MVC-WishMassageGenerator/src/main/java/com/abhi/controller/WishMessageOperationController.java
@@ -1,5 +1,7 @@
 package com.abhi.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.Map;
 
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.abhi.service.IWishMessageGenerator;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class WishMessageOperationController {
@@ -22,6 +26,10 @@ public class WishMessageOperationController {
 		return "welcome";
 	}
 
+	/**
+	 * (Bad Practice)Working with ModelAndView as return type of method as
+	 * ModelAndView
+	 */
 	/*@RequestMapping("/wmg")
 	public ModelAndView fetchMessage(@RequestParam("name")String name) {
 	    String generatedMessage = service.generateMessage(name);
@@ -33,6 +41,10 @@ public class WishMessageOperationController {
 	    return mvw;
 	}*/
 
+	/**
+	 * (Bad Practice) working with Model as a method parameter
+	 */
+
 	/*@RequestMapping("/wmg")
 	public String fetchMessage(@RequestParam("name") String name, Model model) {
 		
@@ -43,6 +55,10 @@ public class WishMessageOperationController {
 		return "checkStatus";
 	}*/
 
+	/**
+	 * (Bad Practice) working with HashMap<String, Object> as a method parameter
+	 */
+
 	/*	@RequestMapping("/wmg")
 		public String fetchMessage(@RequestParam("name") String name,HashMap<String, Object> mp) {
 			System.err.println(mp.getClass());
@@ -52,6 +68,52 @@ public class WishMessageOperationController {
 			return "checkStatus";
 		}*/
 
+	/**
+	 * working with Method as a void with Map<K, V> as a method parameter
+	 * 
+	 * => it will take request path "wmg"as a logical view name
+	 */
+
+	/*	@RequestMapping("/wmg")
+		public void fetchMessage(@RequestParam("name") String name, Map<String, Object> mp) {
+			System.err.println(mp.getClass());
+			String generatedMessage = service.generateMessage(name);
+			mp.put("wMsg", generatedMessage);
+			mp.put("sysdate", new Date());
+		}*/
+
+	/**
+	 * working with Method return type as null with Map<K, V> as a method parameter
+	 * 
+	 * => it will take request path "wmg"as a logical view name
+	 */
+
+	/*@RequestMapping("/wmg")
+	public String fetchMessage(@RequestParam("name") String name, Map<String, Object> mp) {
+		System.err.println(mp.getClass());
+		String generatedMessage = service.generateMessage(name);
+		mp.put("wMsg", generatedMessage);
+		mp.put("sysdate", new Date());
+		return null;
+	}*/
+
+	/**
+	 * (Bad Practice) we can directly give the response to through DispatcherServlet
+	 * to the controller class without involving view resolver or view component
+	 */
+	/*@RequestMapping("/wmg")
+	public void fetchMessage(@RequestParam("name") String name, Map<String, Object> mp, HttpServletResponse response)
+			throws IOException {
+		System.err.println(mp.getClass());
+		PrintWriter pw = response.getWriter();
+		String generatedMessage = service.generateMessage(name);
+		pw.println("Wish Massage:: " + generatedMessage);
+		pw.println("Generate current time and date :: " + new Date());
+	}*/
+
+	/**
+	 * (Best Practice) working with Map<K, V> as a method parameter
+	 */
 	@RequestMapping("/wmg")
 	public String fetchMessage(@RequestParam("name") String name, Map<String, Object> mp) {
 		System.err.println(mp.getClass());
